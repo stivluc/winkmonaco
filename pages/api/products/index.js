fa4731ece0f59db96dcdf801288c6eb1ee2ad5a1
Steven LUCAS <steven.lucas@viacesi.fr>
2024-05-26 16:11:01 +0200

    no generator products

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,3 +1,30 @@
-import { productsAPIHandler } from "@/schemas/productSchema";
+import { ProductModel } from '@/schemas/productSchema';
+import { dbConnect, dbDisconnect } from '@/lib/dbConnect';
 
-export default productsAPIHandler;
+export default async function handler(req, res) {
+  const { method } = req;
+
+  switch (method) {
+    case 'GET':
+      try {
+        await dbConnect();
+        const models = await ProductModel.find();
+        res.status(200).json({ success: true, data: models });
+      } catch (error) {
+        console.log(error);
+        res.status(400).json({ success: false });
+      }
+      break;
+    case 'POST':
+      try {
+        await dbConnect();
+        const model = new ProductModel(req.body);
+        await model.save();
+        res.status(201).json({ success: true, data: model });
+      } catch (error) {
+        console.log(error);
+        res.status(400).json({ success: false, error });
+      }
+      break;
+  }
+}
